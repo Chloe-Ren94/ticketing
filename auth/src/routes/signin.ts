cec87e663a4082cf7f4743dfa2cefa0722f39326
Chloe-Ren94 <chloeren316@gmail.com>
2024-07-26 22:05:12 -0700

    refactor auth code to a common lib

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { validateRequest } from "../middlewares/validate-request";
-import { User } from "../models/user";
-import { BadRequstError } from "../errors/bad-request-error";
-import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
+import { validateRequest, BadRequstError } from "@chloe_ticketing/common";
+import { User } from "../models/user";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
-import { BadRequstError } from "../errors/bad-request-error";
+import { BadRequstError, validateRequest } from "@chloe_ticketing/common";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
